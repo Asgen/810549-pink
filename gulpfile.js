34d7c4942dd408ab14358904f281f6b10847e184
Henry Tsayukov <genuus1@gmail.com>
2018-11-11 01:22:17 +0300

    Убирает style.css из исходников

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task("images", function(){
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-editor-*.svg,logo-htmlacademy.svg,icon-heart.svg,icon-menu-burger.svg}")
+  return gulp.src("source/img/sprite/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
